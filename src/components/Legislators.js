b85e6ba84ed490b326f310f63bd4570e88873ff4
Colleen Purcell <colleenpurcell12@gmail.om>
2017-03-23 16:31:35 -0700

    fallback for geolocation unavailable

--- src/components/Legislators.js
+++ src/components/Legislators.js
@@ -6,11 +6,13 @@ import key from '../../ProPublicaAPIkey.json'
 class Legislators extends React.Component {
     constructor() {
         super();
-        this.state = { members: [], district: '',  chamber: "House",
-            userResp: '', userSenator1: '', userSenator2: '' };
+        this.state = { zipcode: '', members: [], district: '',  chamber: "House",
+            userResp: '', userSenator1: '', userSenator2: '', provideZipCodeQuery: false };
         this.switchChamber = this.switchChamber.bind(this);
         this.handleClickLegislator = this.handleClickLegislator.bind(this);
         this.handleClickYourLegislator = this.handleClickYourLegislator.bind(this);
+        this.onChangeFindLegislatorsByZip = this.onChangeFindLegislatorsByZip.bind(this);
+
     }
     componentDidMount() {
         fetch(`https://api.propublica.org/congress/v1/115/${this.state.chamber}/members.json`,{method:'GET', headers: {'X-API-Key': key[0].secret} })
@@ -18,37 +20,43 @@ class Legislators extends React.Component {
         .then( info =>  { 
             this.setState({ members: info.results[0].members }); 
         });
-
-        navigator.geolocation.getCurrentPosition((position) => {
-            let {latitude, longitude} = position.coords
-            // console.log("******* User lat lon captured") //is ",latitude,",",longitude)
-            fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${googleKey[0].secret}`)
-            .then(response => response.json())
-            .then( info =>  { 
-                //console.log("** Google api res", info.results[0].formatted_address)
-                let zipcode = info.results[0].formatted_address.split(", ")[2].split(" ")[1]
-                // NESTED FETCH REQ
-                fetch(`http://whoismyrepresentative.com/getall_mems.php?zip=${zipcode}&output=json`)
+        if(navigator.geolocation){
+                navigator.geolocation.getCurrentPosition((position) => {
+                    let {latitude, longitude} = position.coords
+                    // console.log("******* User lat lon captured") //is ",latitude,",",longitude)
+                    fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${googleKey[0].secret}`)
                     .then(response => response.json())
-                    .then( info =>  { let { results } = info;
-                        this.setState({ 
-                            userResp: { name: results[0].name, 
-                                last_name: results[0].name.split(" ")[1],
-                                party: results[0].party[0], state: results[0].state,
-                                district: results[0].district
-                            },
-                            userSenator1: { name: results[1].name, 
-                                last_name: results[1].name.split(" ")[1],
-                                party: results[1].party[0], state: results[1].state
-                            },
-                            userSenator2: { name: results[2].name,
-                                last_name: results[2].name.split(" ")[1], 
-                                party: results[2].party[0], state: results[2].state
-                            } 
-                        }); 
-                });
-            });
-        }); 
+                    .then( info =>  { 
+                        //console.log("** Google api res", info.results[0].formatted_address)
+                        let zipcode = info.results[0].formatted_address.split(", ")[2].split(" ")[1]
+                        // NESTED FETCH REQ
+                        fetch(`http://whoismyrepresentative.com/getall_mems.php?zip=${zipcode}&output=json`)
+                            .then(response => response.json())
+                            .then( info =>  { let { results } = info;
+                                this.setState({ 
+                                    userResp: { name: results[0].name, 
+                                        last_name: results[0].name.split(" ")[1],
+                                        party: results[0].party[0], state: results[0].state,
+                                        district: results[0].district
+                                    },
+                                    userSenator1: { name: results[1].name, 
+                                        last_name: results[1].name.split(" ")[1],
+                                        party: results[1].party[0], state: results[1].state
+                                    },
+                                    userSenator2: { name: results[2].name,
+                                        last_name: results[2].name.split(" ")[1], 
+                                        party: results[2].party[0], state: results[2].state
+                                    } 
+                                }); 
+                        });
+                    });
+                }); 
+            }
+        // else{
+            console.log("geolocation not available")
+            this.setState({ provideZipCodeQuery: true })
+        // }
+        // how to tell if 
     }
     switchChamber(e){
         let chamber = e.target.id        
@@ -71,8 +79,36 @@ class Legislators extends React.Component {
         e.preventDefault();
         browserHistory.push(`/oneLegislator/${thisLeg.last_name}/${chamber}`)
     }
+    onChangeFindLegislatorsByZip(e){
+        let zipcode = e.target.value
+        if(zipcode.length<5){ 
+            this.setState({ zipcode: zipcode });
+        }
+        else if(zipcode.length===5){
+            console.log("about to fetch")
+            fetch(`http://whoismyrepresentative.com/getall_mems.php?zip=${zipcode}&output=json`)
+            .then(response => response.json())
+            .then( info =>  { let { results } = info;
+                this.setState({ 
+                    userResp: { name: results[0].name, 
+                        last_name: results[0].name.split(" ")[1],
+                        party: results[0].party[0], state: results[0].state,
+                        district: results[0].district
+                    },
+                    userSenator1: { name: results[1].name, 
+                        last_name: results[1].name.split(" ")[1],
+                        party: results[1].party[0], state: results[1].state
+                    },
+                    userSenator2: { name: results[2].name,
+                        last_name: results[2].name.split(" ")[1], 
+                        party: results[2].party[0], state: results[2].state
+                    },  provideZipCodeQuery: false
+                }); 
+            });
+        }
+    }
     render() {
-        let {   members, chamber, userResp, userSenator1, userSenator2 } = this.state; 
+        let { zipcode, members, chamber, userResp, userSenator1, userSenator2, provideZipCodeQuery } = this.state; 
         if (!members) {
             return ( <div className="user-page">LOADING members...</div> );
         }
@@ -85,7 +121,7 @@ class Legislators extends React.Component {
                 </div>
 
                 <div className="search_container">
-                    {(userResp)?
+                    {(userResp && !provideZipCodeQuery)?
                             <div className="your_representatives search_description">
                                 <div  className="your_representatives_title">
                                     Your representatives are
@@ -107,6 +143,18 @@ class Legislators extends React.Component {
                                         {userSenator2.name} ({userSenator2.state}-Sen. {userSenator2.party})
                                     </span>
                                  </div>
+                            </div>
+                        : (provideZipCodeQuery)?
+                            <div className="search_container">
+                                <div className="search_description">Search for your legislators by zipcode</div>
+                                    <img className="search_icon" src="https://cdn2.iconfinder.com/data/icons/picons-essentials/57/search-512.png" alt="search"/>
+                                    <input className="search_bar" 
+                                        ref="userInput"
+                                        type='text'
+                                        value={zipcode}
+                                        placeholder=""
+                                        onChange={this.onChangeFindLegislatorsByZip}
+                                         />
                             </div>
                         :
                         <div className="your_representatives your_representatives_title search_description">
